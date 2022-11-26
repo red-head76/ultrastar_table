2a3c9e50530be8f691022c02279f98f87c53cc35
Ginok1 <lennart.koenig@gmx.de>
2022-11-26 18:09:34 +0100

    finish read_from_folder function

--- ultrastar_table/ultrastar_table.py
+++ ultrastar_table/ultrastar_table.py
@@ -3,9 +3,9 @@
 import os
 import pathlib
 import re
+import warnings
 
 import pandas as pd
-
 
 
 class UltrastarTable():
@@ -14,12 +14,12 @@ class UltrastarTable():
 
     def read_from_folder(self, path):
         """Reads all song names from a given folder
-        
+
         Parameters
         ----------
         path : str, pathlib.Path
             path to the folder
-        
+
         Returns
         -------
         pd.DataFrame
@@ -27,21 +27,36 @@ class UltrastarTable():
         """
         path = pathlib.Path(path)
         candidates = os.listdir(path)
-        df = pd.DataFrame(columns=['Artist', 'Title', 'Directory', 'Cover', 'Video', 'Commentary'])
+        columns = ['Artist', 'Title', 'Directory', 'Cover', 'Video', 'Commentary']
+        dfs = []
         for candidate in candidates:
-            files = os.listdir(path / candidate)
-            r = re.compile("\.txt$")
-            newlist = list(filter(r.match, files))
-            if len(newlist) > 1:
-                raise FileNotFoundError(f"There are multiple .txt files in the subfolder {candidate}")
-            with open(path / candidate, "r") as f:
-                data = f.readlines()
-                breakpoint()
+            try:
+                files = os.listdir(path / candidate)
+                # Extract Artist and Title information from .txt file
+                r = re.compile(r".*\.txt$")
+                newlist = list(filter(r.match, files))
+                if len(newlist) > 1:
+                    raise FileNotFoundError("There are multiple .txt files in the" +
+                                            f"subfolder {candidate}")
+                txtfile = newlist[0]
+                with open(path / candidate / txtfile, "r") as f:
+                    data = "".join(f.readlines())
+                artist = re.search(r"(?<=#ARTIST:).*?(?=\n)", data).group(0)
+                title = re.search(r"(?<=#TITLE:).*?(?=\n)", data).group(0)
+                # Check if video is available
+                cover = any([re.search(r"(.*\.jpg)|(.*\.png)", file) for file in files])
+                video = any([re.search(r"(.*\.mp4)|(.*\.avi)", file) for file in files])
+                commentary = None
+
+                dfs.append(pd.DataFrame([dict(zip(columns, [artist, title, candidate, cover,
+                                                            video, commentary]))]))
+            except:
+                warnings.warn(f"{candidate} had wrong format and was skipped.")
+        return pd.concat(dfs, ignore_index=True)
 
 
 if __name__ == "__main__":
     path = pathlib.Path("./testset")
     ust = UltrastarTable()
-    ust.read_from_folder(path)
-
-    
+    df = ust.read_from_folder(path)
+    print(df)
