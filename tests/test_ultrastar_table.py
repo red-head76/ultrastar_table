7e7268798c075f1b9643708f9704a4ef1306a0ec
red-head76 <reddy.uni@t-online.de>
2022-11-26 16:57:41 +0100

    wrote dummy-test

--- tests/test_ultrastar_table.py
+++ tests/test_ultrastar_table.py
@@ -1,4 +1,12 @@
+import ultrastar_table
+import os
 import unittest
 
+
 class TestUtrastarTable(unittest.TestCase):
-    pass
+
+    ust = UltrastarTable()
+
+    def test_read_from_folder(folder="./testset"):
+        df = ust.read_from_folder(folder)
+        self.assertTrue(True)
